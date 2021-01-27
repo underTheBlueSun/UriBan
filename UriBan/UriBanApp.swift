b3cd0eaab91fcca80dcc4f6dfd23757bfdff6286
underTheBlueSun <wind7129@naver.com>
2021-01-27 10:25:10 +0900

    Initial Commit

---
+++ UriBan/UriBanApp.swift
@@ -0,0 +1,17 @@
+//
+//  UriBanApp.swift
+//  UriBan
+//
+//  Created by macbook on 2021/01/27.
+//
+
+import SwiftUI
+
+@main
+struct UriBanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
